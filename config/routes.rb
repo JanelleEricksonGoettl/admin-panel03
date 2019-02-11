7412fa05cf1407d1be3e5076ccb9f3ded31d5308
Janelle Erickson-Goettl <janelle.erickson.goettl@gmail.com>
2019-02-11 17:55:47 -0500

    configured localhost in development env. generated controllers and views for Courses, Cohorts, Teachers, and Students.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'home/index'
+  root to: 'home#index'
+
+  resources :teachers
+  resources :students
+  resources :cohorts
+  resources :courses
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
